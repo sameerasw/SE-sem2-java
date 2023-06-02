c1dcba6e671bc7d63ae6115d1feb5f41320402da
Sameera Sandakelum <68902530+sameerasw@users.noreply.github.com>
2023-06-02 19:29:38 +0530

    completed day 03 ui tuts

--- ui003-tut-03/src/main/java/com/example/ui003tut03/HelloController.java
+++ ui003-tut-03/src/main/java/com/example/ui003tut03/HelloController.java
@@ -1,14 +1,41 @@
 package com.example.ui003tut03;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button clrButton;
+
+    @FXML
+    private TextField firstNumber;
+
+    @FXML
+    private TextField secondNumber;
+
+    @FXML
+    private Button sumButton;
+
+    @FXML
+    private Label sumLabel;
+
+    @FXML
+    void clrButtonClick(ActionEvent event) {
+        firstNumber.clear();
+        secondNumber.clear();
+        sumLabel.setText("Sum: ");
     }
+
+    @FXML
+    void sumButtonClick(ActionEvent event) {
+        int num1 = Integer.parseInt(firstNumber.getText());
+        int num2 = Integer.parseInt(secondNumber.getText());
+        int sum = num1 + num2;
+        sumLabel.setText("Sum: " + sum);
+    }
+
 }
