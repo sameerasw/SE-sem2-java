653743c1c16e75a578d44e84e5cc52917da7af62
Sameera Sandakelum <68902530+sameerasw@users.noreply.github.com>
2023-06-09 19:35:13 +0530

    Added the close function to the menu option

--- ui004-tut/src/main/java/com/example/ui004tut/HelloController.java
+++ ui004-tut/src/main/java/com/example/ui004tut/HelloController.java
@@ -3,10 +3,14 @@ package com.example.ui004tut;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
 public class HelloController {
+
+    @FXML
+    private MenuItem close;
 
     @FXML
     private TextField first;
@@ -26,6 +30,11 @@ public class HelloController {
     @FXML
     void onSubmit(ActionEvent event) {
         output.setText(title.getText() + " " + first.getText() + " " + last.getText());
+    }
+
+    @FXML
+    void onClose(ActionEvent event) {
+        System.exit(0);
     }
 
 }
